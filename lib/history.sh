a199fc7957058f20cd72c01e41d13b6e0361d849
Luis (Suanshuan) <ladominguez12@gmail.com>
2026-07-16 12:04:44 -0600

    Adding features to panda history

--- lib/history.sh
+++ lib/history.sh
@@ -6,10 +6,40 @@ source "$SCRIPT_DIR/common.sh"
 banner
 header "Recent Jobs"
 
+DEFAULT_NUM=20
+SHOW_ALL=false
+NUM=$DEFAULT_NUM
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -n|--num)
+            NUM="$2"
+            shift 2
+            ;;
+        --all)
+            SHOW_ALL=true
+            shift
+            ;;
+        *)
+            echo "Usage: panda history [-n N] [--all]"
+            exit 1
+            ;;
+    esac
+done
+
+if ! [[ "$NUM" =~ ^[0-9]+$ ]]; then
+    echo "Error: -n requires a positive integer."
+    exit 1
+fi
+
 print_history_header
 
+if $SHOW_ALL; then
+    tail -n +2 "$HISTORY_FILE" | tac
+else
+    tail -n +2 "$HISTORY_FILE" | tail -n "$NUM" | tac
+fi |
 
-tail -n +2 "$HISTORY_FILE" | tac |
 while IFS=$'\t' read -r \
     JOBID \
     USER_NAME \
